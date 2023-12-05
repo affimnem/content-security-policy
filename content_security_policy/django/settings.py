aa58933c6b4917e98f6a1b8408a1ef9ef4f4bc7e
Niklas Beierl <22919961+NiklasBeierl@users.noreply.github.com>
2023-12-05 11:32:29 +0100

    WIP: Django integration

--- content_security_policy/django/settings.py
+++ content_security_policy/django/settings.py
@@ -7,7 +7,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -19,7 +18,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -29,6 +27,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "content_security_policy.django",
 ]
 
 MIDDLEWARE = [
@@ -39,6 +38,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "content_security_policy.django.middleware.AutoCSPMiddleware",
 ]
 
 ROOT_URLCONF = "content_security_policy.django.urls"
@@ -61,26 +61,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "content_security_policy.django.wsgi.application"
 
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": ":memory:",
     }
 }
-
-
-# Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
-
-LANGUAGE_CODE = "en-us"
-
-TIME_ZONE = "UTC"
-
-USE_I18N = True
-
-USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -91,3 +77,22 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+from content_security_policy.directives import *
+from content_security_policy.values import *
+from content_security_policy.django.auto_src import AutoHostScriptSrc
+
+
+EXTERNAL_SCRIPTS = [HostSrc("https://code.jquery.com/jquery-3.5.1.js")]
+
+CONTENT_SECURITY_POLICY = [
+    AutoHostScriptSrc(*EXTERNAL_SCRIPTS, watch_apps=INSTALLED_APPS),
+    ScriptSrcAttr(NoneSrc),
+    DefaultSrc(KeywordSource.self),
+    StyleSrc(KeywordSource.self),
+    FrameAncestors(NoneSrc),
+    BaseUri(NoneSrc),
+    ObjectSrc(NoneSrc),
+    ManifestSrc(NoneSrc),
+]
