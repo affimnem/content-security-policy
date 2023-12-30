c3fb5963cd67ea1f2bde8411fb8a9b67a97a5964
Niklas Beierl <22919961+NiklasBeierl@users.noreply.github.com>
2023-12-30 16:58:27 +0100

    New: Additional test cases for parsing

--- content_security_policy/test/test_parsing.py
+++ content_security_policy/test/test_parsing.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 
+from content_security_policy import RequireTrustedTypesFor, TrustedTypesSinkGroup
 from content_security_policy.parse import (
     directive_from_string,
     policy_from_string,
@@ -31,4 +32,55 @@ class DirectiveParsing(TestCase):
     def test_parse_serialize_messy_policy_list(self):
         as_string = "   \n require-trusted-types-for 'script';report-uri /_/somehing/cspreport, script-src 'report-sample' 'nonce-EmTYwW9IZXpvlIOURJMuAQ' 'unsafe-inline';object-src 'none';base-uri 'self';report-uri /_/something/cspreport;worker-src 'self' \t "
         parsed = policy_list_from_string(as_string)
+        self.assertEqual(as_string, str(parsed))
+
+
+class Delimiters(TestCase):
+    def test_trailing_delimiters(self):
+        for as_string in [
+            "default-src 'none'; ",
+            "default-src 'none';",
+            "default-src 'none'",
+        ]:
+            with self.subTest(as_string):
+                parsed = policy_list_from_string(as_string)
+                self.assertEqual(len(parsed), 1)
+                policy = parsed[0]
+                self.assertEqual(len(policy), 1)
+                self.assertEqual(as_string, str(parsed))
+
+    def test_empty_directive(self):
+        as_string = "require-trusted-types-for"
+        parsed = policy_list_from_string(as_string)
+        self.assertEqual(len(parsed), 1)
+        policy = parsed[0]
+        self.assertEqual(len(policy), 1)
+        self.assertEqual(as_string, str(parsed))
+
+
+class Sandbox(TestCase):
+    def test_parse_sandbox(self):
+        as_string = "sandbox allow-popups allow-scripts;"
+        parsed = policy_list_from_string(as_string)
+
+        self.assertEqual(as_string, str(parsed))
+
+
+class TrustedTypes(TestCase):
+    def test_parse_trusted_types(self):
+        as_string = "trusted-types pol1 pol-3 'allow-duplicates';"
+        parsed = policy_list_from_string(as_string)
+        self.assertEqual(as_string, str(parsed))
+
+    def test_parse_require_trusted_types_for(self):
+        as_string = "require-trusted-types-for 'script'"
+        parsed = policy_list_from_string(as_string)
+        self.assertEqual(len(parsed), 1)
+        policy = parsed[0]
+        self.assertEqual(len(policy), 1)
+        directive = policy[RequireTrustedTypesFor]
+        self.assertEqual(len(directive.values), 1)
+        value = directive.values[0]
+        self.assertTrue(isinstance(value, TrustedTypesSinkGroup))
+        self.assertEqual(str(value), "'script'")
         self.assertEqual(as_string, str(parsed))
